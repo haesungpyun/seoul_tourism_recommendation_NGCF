2f70dae1421b58620ff421778aec0839bac3501e
haesungpyun <hacastle12@gmail.com>
2022-02-04 17:28:25 +0900

    "Update"

--- model/experiment.py
+++ model/experiment.py
@@ -97,18 +97,16 @@ class Test():
                                                        node_flag=False)
 
                 all_u_emb, all_i_emb = self.model.all_users_emb, self.model.all_items_emb
-                all_u_emb = all_u_emb[0, :].unsqueeze(0)
-                all_pred_ratings = torch.mm(all_u_emb[0], all_i_emb.T)
+                all_u_emb = all_u_emb[u_id[0], :].unsqueeze(0)
+                all_pred_ratings = torch.mm(all_u_emb, all_i_emb.T)
                 _, all_rank = torch.topk(all_pred_ratings[0], self.ks)
                 all_rec = torch.take(pos_item, pred_rank).cpu().numpy().tolist()
                 # print('recommendations :', all_rec)
-                gt_pos = pos_item[0].item()
-                HR.append(self.hit(gt_item=gt_pos, pred_items=all_rec))
+                gt_rank = pos_item[0].item()
+                HR.append(self.hit(gt_item=gt_rank, pred_items=all_rec))
 
                 pred_ratings = torch.mm(u_embeds, pos_i_embeds.T)
                 _, pred_rank = torch.topk(pred_ratings[0], self.ks)
-                recommends = torch.take(
-                    pos_item, pred_rank).cpu().numpy().tolist()
-                gt_rank = pos_item[0].item()
+                recommends = torch.take(pos_item, pred_rank).cpu().numpy().tolist()
                 NDCG.append(self.Ndcg(gt_item=gt_rank, pred_items=recommends))
         return np.mean(HR), np.mean(NDCG)
